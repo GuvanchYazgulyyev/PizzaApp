ef4e2c25de2b8b69218dd5c3c3d33bca4ae698f4
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2024-05-30 01:23:56 +0300

    Add project files.

---
+++ PizzaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
